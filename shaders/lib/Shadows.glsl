c8ad24ecc0f6bd9ac14e4f69d0f0d83385bb5b8f
MikiP98 <79966387+MikiP98@users.noreply.github.com>
2025-07-10 19:17:23 +0200

    Revert "some lighting balance adjustments. improve SSS within shadowmap range (SSRT shadows). improve shadow bias. improve SSAO at screen edges."
    This reverts commit b43f391703203f936ce14bd786f39ed75bc51474.

--- shaders/lib/Shadows.glsl
+++ shaders/lib/Shadows.glsl
@@ -5,21 +5,25 @@ void GriAndEminShadowFix(
 	inout vec3 WorldPos,
 	vec3 FlatNormal,
 	float VanillaAO,
-	float SkyLightmap,
-	float transition
+	float SkyLightmap
 ){
-	float zoomLevel = 1.0-(transition*transition*transition*transition*0.5+0.5);
-	if(SkyLightmap < 0.1 && isEyeInWater != 1) WorldPos = WorldPos - (	fract(WorldPos+cameraPosition - WorldPos*0.0001)*zoomLevel - zoomLevel*0.5);
-}
 
-void applyShadowBias(inout vec3 projectedShadowPosition, in vec3 playerPos, in vec3 geoNormals){
+	float MinimumValue = 0.05;
 
-	// Calculate the bias size according to the 1:1 ratio of one shadow texel to one full block
-	const float biasSize = (shadowDistance / shadowMapResolution*2.0) * 2.0;
+	// give a tiny boost to the distance mulitplier when shadowmap resolution is below 2048.0
+	// float ResMultiplier = 1.0 + (shadowDistance/8.0)*(1.0 - min(shadowMapResolution,2048)/2048.0)*0.3;
 
-	float biasDistanceFactor = length(projectedShadowPosition.xy);
+	// float DistanceMultiplier = max(1.0 - max(1.0 - length(WorldPos) / shadowDistance, 0.0), MinimumValue) * ResMultiplier;
+	float theDistance = max(1.0 - length(WorldPos) / shadowDistance,0.0);
+	float DistanceMultiplier =  mix(0.5, 0.05, theDistance);
+	float DistanceMultiplier2 = mix(1.0, 0.02, theDistance);
+	
+	vec3 Bias = (FlatNormal * DistanceMultiplier + WsunVec * DistanceMultiplier2);
 
-	biasDistanceFactor = 1.0 + biasDistanceFactor * ((16.0*8.0) / shadowDistance) * 0.1;
+	// stop lightleaking by zooming up, centered on blocks
+	vec2 scale = vec2(0.5); scale.y *= 0.5;
+	vec3 zoomShadow =  scale.y - scale.x * fract(WorldPos + cameraPosition + Bias*scale.y*0.1);
+	if(SkyLightmap < 0.1 && isEyeInWater != 1) Bias = zoomShadow;
 
-	projectedShadowPosition += (mat3(shadowModelView) * geoNormals) * biasSize * 0.15 * biasDistanceFactor;
+	WorldPos += Bias;
 }
